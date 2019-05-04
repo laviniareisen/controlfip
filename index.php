f430f25025048e34402f3750791ac897901e99f5
Lavinia Reisen <laviniareisen10160205@gmail.com>
2019-05-03 21:35:18 -0300

    Arquivo principal do sistema

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="bt-br">
+	<head>
+		<title>Home | ControlFip</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>ControlFip</h1>
+		<h2>Sistema de Controle Financeiro Pessoal</h2>
+
+	</body>
+</html>
